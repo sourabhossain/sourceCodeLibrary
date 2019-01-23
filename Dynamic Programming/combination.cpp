c5ae0373f8556a3d774523146b9510fa9feb49fb
Sourav Hossain <souravhossain421@gmail.com>
2019-01-23 06:53:39 -0800

    combination

---
+++ Dynamic Programming/combination.cpp
@@ -0,0 +1,38 @@
+/**
+ *  Name: Combination
+ *  Author Name: Sourav Hossain
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long table[51][51];
+
+long long nCr(int n, int r) {
+	if(r == 1) {
+		return n;
+	}
+
+	if(n == r) {
+		return 1;
+	}
+
+	if(table[n][r] != -1) {
+		return table[n][r];
+	}
+
+	table[n][r] = nCr(n - 1, r) + nCr(n - 1, r - 1);
+	return table[n][r];
+}
+
+int main(int argc, char const *argv[])
+{
+	memset(table, -1, sizeof(table));
+	int n, r;
+
+	scanf("%d%d", &n, &r);
+	printf("%lld\n", nCr(n, r));
+
+	return 0;
+}
