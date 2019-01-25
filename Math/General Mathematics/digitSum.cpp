dc4ed35d864546e74bdf68f4576445df9056389f
Sourav Hossain <souravhossain421@gmail.com>
2019-01-25 13:25:11 -0800

    digit sum

---
+++ Math/General Mathematics/digitSum.cpp
@@ -0,0 +1,53 @@
+/**
+ *  Name: Digit Sum
+ *  Author Name: Sourav Hossain
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int digitSum(int n) {
+	if(n < 10) {
+		return n;
+	}
+
+	return digitSum(digitSum(n / 10) + n % 10);
+}
+
+/**
+
+int digitSum(int n) {
+	if(n < 10) {
+		return n;
+	}
+
+	return digitSum(n / 10) + n % 10;
+}
+
+**/
+
+/**
+
+int digitSum(int n) {
+	int sum = 0;
+
+	while(n) {
+		sum += (n % 10);
+		n /= 10;
+	}
+
+	return sum;
+}
+
+**/
+
+int main(int argc, char const *argv[])
+{
+	int n;
+
+	cin >> n;
+	cout << digitSum(n) << endl;
+
+	return 0;
+}
