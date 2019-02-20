85d4eb41f6dfd52e0363c33e76b9c2369710a2a6
Sourav Hossain <souravhossain421@gmail.com>
2019-02-19 22:44:08 -0800

    triangle count

---
+++ Math/Geometry/triangleCount.cpp
@@ -0,0 +1,18 @@
+/**
+ *  Name: Triangle Count
+ *  Author Name: Sourav Hossain
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int n;
+
+	scanf("%d", &n);
+	printf("%d\n", (n * (n + 2) * ((n << 1) + 1)) >> 3);
+
+	return 0;
+}
