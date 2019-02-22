614d260789759bdf713469a359a680394097fcfe
Sourav Hossain <souravhossain421@gmail.com>
2019-02-22 14:47:33 -0800

    upper bound searching

--- Algorithm/upperBoundSearching.cpp
+++ Algorithm/upperBoundSearching.cpp
@@ -8,22 +8,19 @@
 using namespace std;
 
 int upperBound(vector<int>& data, int target) {
-    int left = 0, right = data.size() - 1, mid, index = -1;
+    int left = 0, right = data.size(), mid;
 
-    while(left <= right) {
-    	mid = left + (right - left) / 2;
-        
-        if(data[mid] == target) {
-            index = mid;
-            left = mid + 1;
-        } else if(data[mid] < target) {
-    		left = mid + 1;
-    	} else {
-    		right = mid - 1;
-    	}
-    }	
+    while(left < right) {
+        mid = left + (right - left) / 2;
 
-    return index;
+        if(target >= data[mid]) {
+            left = mid + 1; 
+        } else {
+            right = mid;
+        }
+    }
+
+    return left;
 }
 
 int main()
