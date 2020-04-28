34fb2c0bfc906950512ad23b62045efcc66c2ef7
Sourab Hossain <cpsourab@gmail.com>
2020-04-28 21:55:22 +0600

    lcm

--- Dynamic Programming/longestCommonSubsequence.cpp
+++ Dynamic Programming/longestCommonSubsequence.cpp
@@ -12,16 +12,10 @@ int longestCommonSubsequence(string str1, string str2) {
 	int dp[m + 1][n + 1];
 
 	for(int i = 0; i <= m; i++) {
-		dp[i][0] = 0;
-	}
-
-	for(int i = 0; i <= n; i++) {
-		dp[0][i] = 0;
-	}
-
-	for(int i = 1; i <= m; i++) {
-		for(int j = 1; j <= n; j++) {
-			if(str1[i - 1] == str2[j - 1]) {
+		for(int j = 0; j <= n; j++) {
+			if(i == 0 || j == 0) {
+                dp[i][j] = 0;
+            } else if(str1[i - 1] == str2[j - 1]) {
 				dp[i][j] = dp[i - 1][j - 1] + 1;
 			} else {
 				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
