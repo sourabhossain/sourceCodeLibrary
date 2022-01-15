0e2abcc0384b0b62cf7c5d35b2df5415771d22a4
Sourab Hossain <sourabhossain.fci@gmail.com>
2022-01-15 21:18:53 +0600

    DEV: #04 - Merge Sort STL

--- Algorithm/mergeSortSTL.cpp
+++ Algorithm/mergeSortSTL.cpp
@@ -2,3 +2,43 @@
  *  Name: Merge Sort STL
  *  Author Name: Sourab Hossain
  */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename Type>
+void mergeSort(Type left, Type right) {
+    int n = right - left;
+
+    if(n < 2) {
+        return;
+    }
+
+    int mid = n >> 1;
+    mergeSort(left, left + mid);
+    mergeSort(left + mid, right);
+
+    typename remove_reference<decltype(*left)>::type* sth = new typename remove_reference<decltype(*left)>::type[n];
+
+    for(int i = 0; i < n; i++) {
+        sth[i] = *(left + i);
+    }
+
+    merge(sth, sth + mid, sth + mid, sth + n, left);
+    delete[] sth;
+}
+
+int main()
+{
+    vector<int> data = {7, 4, 6, 2, 5};
+
+    mergeSort(data.begin(), data.end());
+
+    for(int item : data) {
+        cout << item << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
