fe750c9a673d5f741fa918eaf39fbc711bc88030
Sourab Hossain <32600240+sourabhossain@users.noreply.github.com>
2022-09-11 14:56:32 +0600

    Update sqrtDouble.cpp

--- Math/General Mathematics/sqrtDouble.cpp
+++ Math/General Mathematics/sqrtDouble.cpp
@@ -7,6 +7,45 @@
 
 using namespace std;
 
+double sqrtPrecision(double squareRoot, int n, int p) {
+    double decimalValue = 0.1;
+    
+    for(int i = 0; i < p; i++) {
+        while(squareRoot * squareRoot <= n) {
+            squareRoot = squareRoot + decimalValue;
+        }
+        
+        squareRoot = squareRoot - decimalValue;
+        decimalValue = decimalValue / 10;
+    }
+    
+    return squareRoot;
+}
+
+double findSquareRoot(int n, int p) {
+    if(n <= 1) {
+        return n;
+    }
+    
+    int low = 1, high = n;
+    
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        
+        if(mid * mid <= n && (mid + 1) * (mid + 1) > n) {
+            return sqrtPrecision((double)mid, n, p);
+        }
+        
+        if(mid * mid <= n) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    
+    return sqrtPrecision(high, n, p);
+}
+
 double mySqrt(double n) {
     if(n == 0) {
     	return 0;
