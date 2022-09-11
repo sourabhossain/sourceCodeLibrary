0c8389dd9ee2a5903fc50173197878595bb8053c
Sourab Hossain <32600240+sourabhossain@users.noreply.github.com>
2022-09-11 13:56:32 +0600

    Update GCD.cpp

--- Math/Number Theory/GCD.cpp
+++ Math/Number Theory/GCD.cpp
@@ -5,6 +5,22 @@
 
 #include <stdio.h>
 
+int gcd(int m, int n) {
+    int minValue = min(m, n);
+    
+    if(m % minValue == 0 && n % minValue == 0) {
+        return minValue;
+    }
+    
+    for(int i = minValue / 2; i >= 2; i--) {
+        if(m % i == 0 && n % i == 0) {
+            return i;
+        }
+    }
+    
+    return 1;
+}
+
 int gcd(int first, int second) {
 	int temp;
 
