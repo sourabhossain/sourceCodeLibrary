e2ee540708b8f47d3480dcb094b288069d8ea1c5
Sourab Hossain <32600240+sourabhossain@users.noreply.github.com>
2022-09-25 00:00:18 +0600

    Update longestCommonSubsequence.cpp

--- Dynamic Programming/longestCommonSubsequence.cpp
+++ Dynamic Programming/longestCommonSubsequence.cpp
@@ -9,21 +9,21 @@ using namespace std;
 
 int longestCommonSubsequence(string str1, string str2) {
     int m = str1.size(), n = str2.size();
-	int dp[m + 1][n + 1];
+    int dp[m + 1][n + 1];
 
-	for(int i = 0; i <= m; i++) {
-		for(int j = 0; j <= n; j++) {
-			if(i == 0 || j == 0) {
+    for(int i = 0; i <= m; i++) {
+        for(int j = 0; j <= n; j++) {
+	    if(i == 0 || j == 0) {
                 dp[i][j] = 0;
             } else if(str1[i - 1] == str2[j - 1]) {
-				dp[i][j] = dp[i - 1][j - 1] + 1;
-			} else {
-				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-			}
-		}
+		dp[i][j] = dp[i - 1][j - 1] + 1;
+	    } else {
+		dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+	    }
 	}
+     }
 
-	return dp[m][n];
+    return dp[m][n];
 }
 
 int main(int argc, char const *argv[])
