e503efb3ecd2565e529b22e29bb5d196a45e0c94
Sourab Hossain <32600240+sourabhossain@users.noreply.github.com>
2023-05-11 11:46:05 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,10 +2,20 @@
 
 using namespace std;
 
+#define BOOLEAN_PRINT cout << boolalpha;
+#define FASTER_RUN ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define IGNORE cin.ignore();
+#define PI acos(-1)
+
+void setIO(const string& file_name) {
+    freopen((file_name + ".in").c_str(), "r", stdin);
+    freopen((file_name + ".out").c_str(), "w", stdout);
+}
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    setIO("problem_name");
+    FASTER_RUN;
 
     cout << "Hello! Sourab ~_~" << endl;
 
